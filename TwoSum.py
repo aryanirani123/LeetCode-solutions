7c7c519a8765f42aacf2cdfc5ef159e97b69623a
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2021-12-29 12:03:23 +0530

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,3 +1,11 @@
+"""
+Problem 1 : Given an array of integers nums and an integer target, 
+            return indices of the two numbers such that they 
+            add up to target.
+
+"""
+
+
 class Solution:
     def twoSum(self, nums, target):
         """
